d60fce8072d4867fb2e928a8c691a3a63fe9121d
httpsyaseen <yaseenwalker1@gmail.com>
2024-01-31 18:24:46 +0500

    successfully fetch data by useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loaidng",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "getData":
+      return { ...state, questions: action.payload };
+    case "failedData":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Now Action is not supported");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8080/questionss")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "getData", payload: data }))
+      .catch((err) => dispatch({ type: "failedData" }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main className="main">
+      <p>1/15</p>
+      <p>Question? </p>
+    </main>
+  );
+}
